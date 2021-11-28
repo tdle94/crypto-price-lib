0e174d1e3df2ef001684e6efb9ae6ef3198926c4
Tuyen Le <electro@Tuyens-iMac.local>
2021-11-27 19:22:49 -0500

    add throw error

--- src/crypto-currency.js
+++ src/crypto-currency.js
@@ -2,14 +2,22 @@ export default class CryptoCurrency {
     async getAll() {
         const options = { method: "GET", headers: { Accept: "application/json" } };
 
-        return await fetch("https://api.exchange.coinbase.com/currencies", options)    
-        .then(response => response.json());
+        try {
+            return await fetch("https://api.exchange.coinbase.com/currencies", options)    
+            .then(response => response.json());
+        } catch (error) {
+            return Promise.reject(Error(error));
+        }
     }
 
     async get(currencyId) {
         const options = {method: "GET", headers: {Accept: "application/json"}};
 
-        return await fetch(`https://api.exchange.coinbase.com/currencies/${currencyId}`, options)
-        .then(response => response.json());
+        try {
+            return await fetch(`https://api.exchange.coinbase.com/currencies/${currencyId}`, options)
+            .then(response => response.json());
+        } catch (error) {
+            return Promise.reject(Error(error));
+        }
     }
 }
