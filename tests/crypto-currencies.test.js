7179b14943eb49ac1365a8e00e7b8e02926b3e48
Tuyen Le <electro@Tuyens-iMac.local>
2021-11-27 19:24:40 -0500

    update tests

--- tests/crypto-currencies.test.js
+++ tests/crypto-currencies.test.js
@@ -1,22 +1,43 @@
 import CryptoCurrency from "../src/crypto-currency";
-import data from "./datas/crypto-currency";
+import gtcData from "./datas/gtc-currency";
+import cryptoData from "./datas/crypto-currency";
 
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () => Promise.resolve(data),
-  })
-);
+jest.mock("../src/crypto-currency");
 
 describe("all crypto currencies", () => {
     beforeEach(() => {
-        fetch.mockClear();
+        CryptoCurrency.mockImplementation(() => {
+          return {
+              getAll: () => {
+                  return Promise.resolve(cryptoData);
+              }
+          };
+      });
     });
     
     test("fetch all crypto currencies available", () => {
         const crypto = new CryptoCurrency();
         return crypto.getAll().then(data => {
-            expect(data[0].id).toEqual("DASH")
+            expect(data[0].id).toEqual("DASH");
         });
     });
 });
 
+describe("gtc currency", () => {
+  beforeEach(() => {
+      CryptoCurrency.mockImplementation(() => {
+          return {
+              get: () => {
+                  return Promise.resolve(gtcData);
+              }
+          };
+      });
+  });
+  
+  test("fetch gtc currency", () => {
+      const crypto = new CryptoCurrency();
+      return crypto.get("GTC").then(data => {
+          expect(data.id).toEqual("GTC");
+      });
+  });
+});
