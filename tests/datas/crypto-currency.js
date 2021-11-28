95460514583ed9b612a7255062027e5e51558a10
Tuyen Le <electro@Tuyens-iMac.local>
2021-11-28 11:55:18 -0500

    change to single quotes

--- tests/datas/crypto-currency.js
+++ tests/datas/crypto-currency.js
@@ -1,52 +1,52 @@
 export default [
     {
-      "id": "DASH",
-      "name": "Dash",
-      "min_size": "1",
-      "status": "online",
-      "message": "",
-      "max_precision": "0.00000001",
-      "convertible_to": [],
-      "details": {
-        "type": "crypto",
-        "symbol": null,
-        "network_confirmations": 2,
-        "sort_order": 47,
-        "crypto_address_link": "https://chain.so/address/DASH/{{address}}",
-        "crypto_transaction_link": "https://chain.so/tx/DASH/{{address}}",
-        "push_payment_methods": [
-          "crypto"
+      'id': 'DASH',
+      'name': 'Dash',
+      'min_size': '1',
+      'status': 'online',
+      'message': '',
+      'max_precision': '0.00000001',
+      'convertible_to': [],
+      'details': {
+        'type': 'crypto',
+        'symbol': null,
+        'network_confirmations': 2,
+        'sort_order': 47,
+        'crypto_address_link': 'https://chain.so/address/DASH/{{address}}',
+        'crypto_transaction_link': 'https://chain.so/tx/DASH/{{address}}',
+        'push_payment_methods': [
+          'crypto'
         ],
-        "group_types": [],
-        "display_name": null,
-        "processing_time_seconds": null,
-        "min_withdrawal_amount": 0.0001,
-        "max_withdrawal_amount": 5900
+        'group_types': [],
+        'display_name': null,
+        'processing_time_seconds': null,
+        'min_withdrawal_amount': 0.0001,
+        'max_withdrawal_amount': 5900
       }
     },
     {
-      "id": "YFI",
-      "name": "yearn.finance",
-      "min_size": "0.00001",
-      "status": "online",
-      "message": "We've started accepting yearn.finance deposits. Trading will begin shortly.",
-      "max_precision": "0.000001",
-      "convertible_to": [],
-      "details": {
-        "type": "crypto",
-        "symbol": null,
-        "network_confirmations": 35,
-        "sort_order": 215,
-        "crypto_address_link": "https://etherscan.io/token/0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e?a={{address}}",
-        "crypto_transaction_link": "https://etherscan.io/tx/0x{{txId}}",
-        "push_payment_methods": [
-          "crypto"
+      'id': 'YFI',
+      'name': 'yearn.finance',
+      'min_size': '0.00001',
+      'status': 'online',
+      'message': 'We\'ve started accepting yearn.finance deposits. Trading will begin shortly.',
+      'max_precision': '0.000001',
+      'convertible_to': [],
+      'details': {
+        'type': 'crypto',
+        'symbol': null,
+        'network_confirmations': 35,
+        'sort_order': 215,
+        'crypto_address_link': 'https://etherscan.io/token/0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e?a={{address}}',
+        'crypto_transaction_link': 'https://etherscan.io/tx/0x{{txId}}',
+        'push_payment_methods': [
+          'crypto'
         ],
-        "group_types": [],
-        "display_name": null,
-        "processing_time_seconds": null,
-        "min_withdrawal_amount": 0.0002,
-        "max_withdrawal_amount": 20
+        'group_types': [],
+        'display_name': null,
+        'processing_time_seconds': null,
+        'min_withdrawal_amount': 0.0002,
+        'max_withdrawal_amount': 20
       }
     }
   ];
--- tests/datas/gtc-currency.js
+++ tests/datas/gtc-currency.js
@@ -1,25 +1,25 @@
 export default {
-    "id": "GTC",
-    "name": "Gitcoin",
-    "min_size": "0.1",
-    "status": "online",
-    "message": "",
-    "max_precision": "0.01",
-    "convertible_to": [],
-    "details": {
-      "type": "crypto",
-      "symbol": null,
-      "network_confirmations": 35,
-      "sort_order": 360,
-      "crypto_address_link": "https://etherscan.io/token/0xde30da39c46104798bb5aa3fe8b9e0e1f348163f?a={{address}}",
-      "crypto_transaction_link": "https://etherscan.io/tx/0x{{txId}}",
-      "push_payment_methods": [
-        "crypto"
+    'id': 'GTC',
+    'name': 'Gitcoin',
+    'min_size': '0.1',
+    'status': 'online',
+    'message': '',
+    'max_precision': '0.01',
+    'convertible_to': [],
+    'details': {
+      'type': 'crypto',
+      'symbol': null,
+      'network_confirmations': 35,
+      'sort_order': 360,
+      'crypto_address_link': 'https://etherscan.io/token/0xde30da39c46104798bb5aa3fe8b9e0e1f348163f?a={{address}}',
+      'crypto_transaction_link': 'https://etherscan.io/tx/0x{{txId}}',
+      'push_payment_methods': [
+        'crypto'
       ],
-      "group_types": [],
-      "display_name": null,
-      "processing_time_seconds": null,
-      "min_withdrawal_amount": 1e-18,
-      "max_withdrawal_amount": 20700
+      'group_types': [],
+      'display_name': null,
+      'processing_time_seconds': null,
+      'min_withdrawal_amount': 1e-18,
+      'max_withdrawal_amount': 20700
     }
   };
