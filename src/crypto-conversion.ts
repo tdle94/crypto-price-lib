ade265904d1241cdc6272e363458aa84f13a5271
Tuyen Le <electro@Tuyens-iMac.local>
2021-11-29 17:02:04 -0500

    add Crypto Conversion class

--- src/crypto-conversion.ts
+++ src/crypto-conversion.ts
@@ -1,8 +1,54 @@
 import request from './request';
 import Base from './base';
 
-class CryptoConversion {
-    async convertCurrency(profileId: number, from: string, to: string, amount: string, nonce: string) {
+export default class CryptoConversion extends Base {
+    async convertCurrency(from: string, to: string, amount: string, profileId?: string, nonce?: string) {
+        const method = 'POST';
+        const timestamp = Date.now();
+        const requestPath = `/conversions`;
 
+        this.configureHeaderOptions(method, timestamp, requestPath);
+
+        const requestBody: CryptoConversion.ConvertCurrencyBody = {
+            from,
+            to,
+            amount,
+            nonce,
+            profile_id: profileId
+        }
+
+        this.options.body = JSON.stringify(requestBody);
+        
+        return request(this.urlRequest.getURLString(), this.options);
+    }
+
+    async getAConversion(conversionId: string, profileId?: string) {
+        const method = 'GET';
+        const timestamp = Date.now();
+        const requestPath = `/conversions/${conversionId}`;
+
+        this.configureHeaderOptions(method, timestamp, requestPath);
+
+        const requestBody: CryptoConversion.GetAConversionBody = {
+            profile_id: profileId
+        }
+
+        this.options.body = JSON.stringify(requestBody);
+
+        return request(this.urlRequest.getURLString(), this.options);
+    }
+}
+
+namespace CryptoConversion {
+    export interface ConvertCurrencyBody {
+        from: string,
+        to: string,
+        amount: string,
+        profile_id?: string,
+        nonce?: string
+    }
+
+    export interface GetAConversionBody {
+        profile_id?: string,
     }
 }
