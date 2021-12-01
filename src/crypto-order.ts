7cc4c55fc6a381f12fc581b898e7da8eaf81f765
Tuyen Le <electro@Tuyens-iMac.local>
2021-11-30 22:52:37 -0500

    add crypto order API

---
+++ src/crypto-order.ts
@@ -0,0 +1,62 @@
+import { URLQueryItem } from './url-request';
+import request from './request';
+import Base from './base';
+
+export default class CryptoOrder extends Base {
+    async getAllFills(orderId?: string, productId?: string, profileId?: string, limit?: number, before?: number, after?: number) {
+        const method = 'POST';
+        const timestamp = Date.now();
+        const requestPath = '/fills';
+
+        const queryItems = [
+            new URLQueryItem('order_id', orderId),
+            new URLQueryItem('product_id', productId),
+            new URLQueryItem('profile_id', profileId),
+            new URLQueryItem('limit', limit),
+            new URLQueryItem('before', before),
+            new URLQueryItem('after', after)
+        ];
+
+        this.configureHeaderOptions(method, timestamp, requestPath, queryItems);
+
+        return request(this.urlRequest.getURLString(), this.options);
+    }
+
+    async getAllOrders(limit: number, status: [string], profileId?: string, productId?: string, startDate?: Date, endDate?: Date, before?: string, after?: string, sortedBy: string = 'created_at', sorting: string = 'desc',) {
+        const method = 'POST';
+        const timestamp = Date.now();
+        const requestPath = '/orders';
+
+        const queryItems = [
+            new URLQueryItem('limit', limit),
+            new URLQueryItem('status', status),
+            new URLQueryItem('profileId', profileId),
+            new URLQueryItem('productId', productId),
+            new URLQueryItem('startDate', startDate.toISOString()),
+            new URLQueryItem('endDate', endDate.toISOString()),
+            new URLQueryItem('before', before),
+            new URLQueryItem('after', after),
+            new URLQueryItem('sortedBy', sortedBy),
+            new URLQueryItem('sorting', sorting)
+        ];
+        
+        this.configureHeaderOptions(method, timestamp, requestPath, queryItems);
+
+        return request(this.urlRequest.getURLString(), this.options);
+    }
+
+    async cancelAllOrders(profileId?: string, productId?: string) {
+        const method = 'DELETE';
+        const timestamp = Date.now();
+        const requestPath = '/orders';
+
+        const queryItems = [
+            new URLQueryItem('product_id', productId),
+            new URLQueryItem('profile_id', profileId)
+        ]
+
+        this.configureHeaderOptions(method, timestamp, requestPath, queryItems);
+
+        return request(this.urlRequest.getURLString(), this.options);
+    }
+}
